ebe4ab52577c4c74a6d3e7ccabe913ea5931adcf
yuch125 <yuchanb35@gmail.com>
2026-09-18 11:11:31 +0900

    집합

--- 04_collection/ex6.py
+++ 04_collection/ex6.py
@@ -77,9 +77,21 @@ words = ["apple", "banana", "apple", "cherry", "banana", "apple"]
 result = {x: words.count(x) for x in words}
 print(result)
 
+# 1) 클래식 for
 count = {}
 for word in words:
     count[word] = count.get(word, 0) + 1
+
+# 2) dict 컴프리헨션
+result = {x: words.count(x) for x in set(words)}
+print(result)
+
+# 3) Counter: 요소 갯수를 자동으로 세어주는 딕셔너리 서브클래스
+from collections import Counter
+
+print(dict(Counter(words)))
+
+
 # ✅ {'apple': 3, 'banana': 2, 'cherry': 1}
 
 
@@ -102,7 +114,19 @@ print(dict(zip(subjects, grades)))
 stock = {"연필": 10, "지우개": 5, "노트": 3}  # 기존 재고
 incoming = {"지우개": 4, "노트": 7, "볼펜": 12}  # 입고 내역
 
+# 1) 클래식 for
 for item, qty in incoming.items():
     stock[item] = stock.get(item, 0) + qty
 print(stock)
+
+# 2) dict 컴프리헨션
+# stock.update(
+#     {
+#         item: stock[item] + qty if item in stock else qty
+#         for item, qty in incoming.items()
+#     }
+# )
+# print(stock)
+# stock.update({item: stock.get(item, 0) + qty for item, qty in incoming.items()})
+# print(stock)
 # ✅ {'연필': 10, '지우개': 9, '노트': 10, '볼펜': 12}
